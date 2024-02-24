296a09b1b482a65a44c1b5ba09b587225333975a
aic-6301 <rx435423@gmail.com>
2024-02-24 17:12:25 +0900

    クラスと関数名を少し短く

--- HSS.py
+++ HSS.py
@@ -1,36 +1,24 @@
-import errors
-import apiurl_lists
-import Request_HSSAPI
+from . import errors, apiurl_lists, Request_HSSAPI
 
-class HSSGetPermissonClass:
+class GetUserData:
     def __init__(self,token) -> None:
         self.toke = token
 
-    async def get_data(self) -> dict:
-        url = await apiurl_lists.make_url(2)
+    async def get_data(self, url) -> dict:
         response = await Request_HSSAPI.send_request_with_token(url, self.toke)
         if await errors.ErrorPrint.handle_http_error(response):
             return None
         return response.json()
     
     async def get_permission(self) -> list:
-        UserData = await self.get_data()
+        url = await apiurl_lists.make_url(2)
+        UserData = await self.get_data(url)
         if UserData['body']['schools'] == []:
             return None
         return UserData['body']['schools']
 
-class HSSGetUserDatasClass:
-    def __init__(self,token) -> None:
-        self.toke = token
 
-    async def get_data(self,url) -> dict:
-        response = await Request_HSSAPI.send_request_with_token(url, self.toke)
-        if await errors.ErrorPrint.handle_http_error(response):
-            return None
-        return response.json()
-
-
-    async def GetIdData(self,id) -> int:
+    async def get_id(self,id) -> int:
         url = await apiurl_lists.make_url(1,id)
         UserData = await self.get_data(url)
         if UserData['body']['data'] == None:
@@ -38,17 +26,16 @@ class HSSGetUserDatasClass:
         return UserData['body']['data']
     
 
-    async def GetmeData(self) -> dict:
+    async def get_me(self) -> dict:
         url = await apiurl_lists.make_url(1,"@me")
         UserData = await self.get_data(url)
         return UserData['body']['data']
 
-class HSSGetSchoolDatasClass:
+class GetSchoolData:
     def __init__(self,token,schoolid) -> None:
         self.DayOfWeek = ["sun","mon","tue","wed","thu","fri","sat"]
         self.toke = token
         self.schoolid = schoolid
-        
 
     async def get_data(self) -> dict:
         url = await apiurl_lists.make_url(0,self.schoolid)
@@ -58,27 +45,6 @@ class HSSGetSchoolDatasClass:
         UserData = response.json()
         return UserData['body']['data']
 
-    {'schoolId': '6382837564960670720',
-    'details': 
-    {
-        'type': '0', 
-        'ownerId': '6381316455702463488', 
-        'admins': ['6381316455702463488'], 
-        'name': 'NIT OITA', 
-        'id': '6382837564960670720', 
-        'timelineDefaultIndexs': 6
-    },
-        'userDatas': [
-            {
-                'defaultTimelineIndex': 4, 
-                'grade': 1, 
-                'class': 1, 
-                'timelineData': {'sun': [], 'mon': [], 'tue': [], 'wed': [], 'thu': [], 'fri': [], 'sat': []},
-                'eventData': {'sun': [], 'mon': [], 'tue': [], 'wed': [], 'thu': [], 'fri': [], 'sat': []},
-                'defaultTimelineData': {'sun': [{'name': '初期の値'}], 'mon': [{'name': '物理', 'place': '初期値', 'homework': [], 'IsEvent': False}, {'name': '物理', 'place': '初期値', 'homework': [], 'IsEvent': False}, {'name': '物理', 'place': '初期値', 'homework': [], 'IsEvent': False}, {'name': '物理', 'place': '初期値', 'homework': [], 'IsEvent': False}], 'tue': [], 'wed': [], 'thu': [], 'fri': [], 'sat': []}
-            }
-        ]
-    }
     
     async def search_class(self,grade,classname) -> int:
         UserData = await self.get_data()
@@ -107,7 +73,7 @@ class HSSGetSchoolDatasClass:
             return None
         return UserData['class']
     
-    async def DayOfWeek_timelineData(self,number,name) -> list[dict]:
+    async def get_timeline(self,number,name) -> list[dict]:
         if name not in self.DayOfWeek:
             return None
         UserData = await self.get_data()
@@ -118,7 +84,7 @@ class HSSGetSchoolDatasClass:
         UserData = UserData['userDatas'][number]
         return UserData['timelineData'][name]
     
-    async def DayOfWeek_timelineData_default(self,number,name) -> list[dict]:
+    async def get_default_timeline(self,number,name) -> list[dict]:
         if name not in self.DayOfWeek:
             return None
         UserData = await self.get_data()
@@ -127,14 +93,14 @@ class HSSGetSchoolDatasClass:
         UserData = UserData['userDatas'][number]
         return UserData['defaultTimelineData'][name]
     
-    async def eventData(self,number,name) -> list[dict]:
+    async def get_event(self,number,name) -> list[dict]:
         UserData = await self.get_data()
         if UserData['userDatas'] == []:
             return None
         UserData = UserData['userDatas'][number]
         return UserData['eventData'][name]
 
-    async def defaultTimelineIndex(self,number) -> int:
+    async def default_TimelineIndex(self,number) -> int:
         UserData = await self.get_data()
         if UserData['userDatas'] == []:
             return None
