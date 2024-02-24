b8b1f735fd1619949a301925db4a74c91e3b380c
aic-6301 <rx435423@gmail.com>
2024-02-24 17:25:04 +0900

    [Add] Requestの種類を追加

--- Request_HSSAPI.py
+++ Request_HSSAPI.py
@@ -1,11 +1,32 @@
 import requests
 
 
-async def send_request_with_token(url, token):
+async def get_with_token(url, token):
     headers = {
         'Authorization':f"Bearer {token}"
     }
     response = requests.get(url, headers=headers)
+    return response
+
+async def patch_with_token(url, token):
+    headers = {
+        'Authorization':f"Bearer {token}"
+    }
+    response = requests.patch(url, headers=headers)
+    return response
+
+async def post_with_token(url, token):
+    headers = {
+        'Authorization':f"Bearer {token}"
+    }
+    response = requests.post(url, headers=headers)
+    return response
+
+async def delete_with_token(url, token):
+    headers = {
+        'Authorization':f"Bearer {token}"
+    }
+    response = requests.delete(url, headers=headers)
     return response
 
 
