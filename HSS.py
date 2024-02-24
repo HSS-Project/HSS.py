aafdad4cf2beec19031c3a1c8f0d22a9ee18fe9a
aic-6301 <rx435423@gmail.com>
2024-02-24 18:51:35 +0900

    asyncの削除 (今後必要だったらつけてください((()

--- HSS.py
+++ HSS.py
@@ -4,31 +4,31 @@ class User:
     def __init__(self,token) -> None:
         self.toke = token
 
-    async def get_data(self, url) -> dict:
+    def get_data(self, url) -> dict:
         response = Request_HSSAPI.get_with_token(url, self.toke)
         if errors.ErrorPrint.handle_http_error(response):
             return None
         return response.json()
     
-    async def get_permission(self) -> list:
+    def get_permission(self) -> list:
         url = apiurl_lists.make_url(2)
-        UserData = await self.get_data(url)
+        UserData = self.get_data(url)
         if UserData['body']['schools'] == []:
             return None
         return UserData['body']['schools']
 
 
-    async def get_id(self,id) -> int:
+    def get_id(self,id) -> int:
         url = apiurl_lists.make_url(1,id)
-        UserData = await self.get_data(url)
+        UserData = self.get_data(url)
         if UserData['body']['data'] == None:
             return None
         return UserData['body']['data']
     
 
-    async def get_me(self) -> dict:
+    def get_me(self) -> dict:
         url = apiurl_lists.make_url(1,"@me")
-        UserData = await self.get_data(url)
+        UserData = self.get_data(url)
         return UserData['body']['data']
 
 class School:
@@ -37,7 +37,7 @@ class School:
         self.toke = token
         self.schoolid = schoolid
 
-    async def get_data(self) -> dict:
+    def get_data(self) -> dict:
         url = apiurl_lists.make_url(0,self.schoolid)
         response = Request_HSSAPI.get_with_token(url, self.toke)
         if errors.ErrorPrint.handle_http_error(response):
@@ -46,8 +46,8 @@ class School:
         return UserData['body']['data']
 
     
-    async def search_class(self,grade,classname) -> int:
-        UserData = await self.get_data()
+    def search_class(self,grade,classname) -> int:
+        UserData = self.get_data()
         if UserData['userDatas'] == []:
             return None
         for number in range(len(UserData['userDatas'])):
@@ -55,8 +55,8 @@ class School:
                 return number    
         else:
             return None
-    async def grade(self,number) -> int:
-        UserData = await self.get_data()
+    def grade(self,number) -> int:
+        UserData = self.get_data()
         if UserData['userDatas'] == []:
             return None
         UserData = UserData['userDatas'][number]
@@ -64,8 +64,8 @@ class School:
             return None
         return UserData['grade']
     
-    async def classname(self,number) -> str:
-        UserData = await self.get_data()
+    def classname(self,number) -> str:
+        UserData = self.get_data()
         if UserData['userDatas'] == []:
             return None
         UserData = UserData['userDatas'][number]
@@ -73,10 +73,10 @@ class School:
             return None
         return UserData['class']
     
-    async def get_timeline(self,number,name) -> list[dict]:
+    def get_timeline(self,number,name) -> list[dict]:
         if name not in self.DayOfWeek:
             return None
-        UserData = await self.get_data()
+        UserData = self.get_data()
         if UserData['userDatas'] == []:
             return None
         if UserData['userDatas'][number]['timelineData'] == None:
@@ -84,24 +84,24 @@ class School:
         UserData = UserData['userDatas'][number]
         return UserData['timelineData'][name]
     
-    async def get_default_timeline(self,number,name) -> list[dict]:
+    def get_default_timeline(self,number,name) -> list[dict]:
         if name not in self.DayOfWeek:
             return None
-        UserData = await self.get_data()
+        UserData = self.get_data()
         if UserData['userDatas'] == []:
             return None
         UserData = UserData['userDatas'][number]
         return UserData['defaultTimelineData'][name]
     
-    async def get_event(self,number,name) -> list[dict]:
-        UserData = await self.get_data()
+    def get_event(self,number,name) -> list[dict]:
+        UserData = self.get_data()
         if UserData['userDatas'] == []:
             return None
         UserData = UserData['userDatas'][number]
         return UserData['eventData'][name]
 
-    async def default_TimelineIndex(self,number) -> int:
-        UserData = await self.get_data()
+    def default_TimelineIndex(self,number) -> int:
+        UserData = self.get_data()
         if UserData['userDatas'] == []:
             return None
         UserData = UserData['userDatas'][number]
